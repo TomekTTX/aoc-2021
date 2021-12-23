0874c8a756d6f31d23403a7f00f9a7dfc1811652
TomekTTX <tomekttx@gmail.com>
2021-12-23 20:42:26 +0100

    Replace tabs with spaces v2

--- 11.1.cpp
+++ 11.1.cpp
@@ -4,119 +4,119 @@
 constexpr const int size = 10;
 
 struct Point {
-	int x, y;
-	
-	std::array<Point, 8> neighbours() const {
-		return {{
-			{x - 1, y - 1},
-			{x    , y - 1},
-			{x + 1, y - 1},
-			{x - 1, y    },
-			{x + 1, y    },
-			{x - 1, y + 1},
-			{x    , y + 1},
-			{x + 1, y + 1},
-		}};
-	}
-	
+    int x, y;
+    
+    std::array<Point, 8> neighbours() const {
+        return {{
+            {x - 1, y - 1},
+            {x    , y - 1},
+            {x + 1, y - 1},
+            {x - 1, y    },
+            {x + 1, y    },
+            {x - 1, y + 1},
+            {x    , y + 1},
+            {x + 1, y + 1},
+        }};
+    }
+    
 };
 
 struct Octopus {
-	short energy;
-	bool flashed = false;
-	
-	void increment() {
-		if (!flashed)
-			energy++;
-	}
+    short energy;
+    bool flashed = false;
+    
+    void increment() {
+        if (!flashed)
+            energy++;
+    }
 };
 
 class OctopusMatrix {
 private:
-	int total_flashes = 0;
-	std::array<Octopus, size * size> vals{};
+    int total_flashes = 0;
+    std::array<Octopus, size * size> vals{};
 public:
-	inline int total() const { return total_flashes; }
+    inline int total() const { return total_flashes; }
 
-	void step() {
-		int flashes;
-		
-		for (auto &v : vals)
-			v.energy += 1;
-		while ((flashes = do_flashes()))
-			total_flashes += flashes;		
-		for (auto &v : vals)
-			v.flashed = false;
-	}
-	
-	void step(unsigned int count, bool log = false) {		
-		if (log) std::cout << "\nStep 0:" << *this << '\n';
-		for (unsigned int i = 0; i < count; ++i) {
-			step();
-			if (log) std::cout << "\nStep " << i + 1 << ':' << *this << '\n';
-		}
-	}
-	
-	friend std::istream& operator>>(std::istream &ist, OctopusMatrix &om) {
-		char c;
-		
-		for (int i = 0; i < (int)om.vals.size(); ++i) {
-			ist >> c;
-			om.vals[i] = Octopus(c & 0xF);
-		}			
-		
-		return ist;
-	}
-	
-	friend std::ostream& operator<<(std::ostream &ost, const OctopusMatrix &om) {
-		for (int i = 0; i < (int)om.vals.size(); ++i) {
-			if (i % size == 0)
-				ost << '\n';
-			ost << om.vals[i].energy;
-		}			
-		
-		ost << '\n';
-		return ost;
-	}
+    void step() {
+        int flashes;
+        
+        for (auto &v : vals)
+            v.energy += 1;
+        while ((flashes = do_flashes()))
+            total_flashes += flashes;       
+        for (auto &v : vals)
+            v.flashed = false;
+    }
+    
+    void step(unsigned int count, bool log = false) {       
+        if (log) std::cout << "\nStep 0:" << *this << '\n';
+        for (unsigned int i = 0; i < count; ++i) {
+            step();
+            if (log) std::cout << "\nStep " << i + 1 << ':' << *this << '\n';
+        }
+    }
+    
+    friend std::istream& operator>>(std::istream &ist, OctopusMatrix &om) {
+        char c;
+        
+        for (int i = 0; i < (int)om.vals.size(); ++i) {
+            ist >> c;
+            om.vals[i] = Octopus(c & 0xF);
+        }           
+        
+        return ist;
+    }
+    
+    friend std::ostream& operator<<(std::ostream &ost, const OctopusMatrix &om) {
+        for (int i = 0; i < (int)om.vals.size(); ++i) {
+            if (i % size == 0)
+                ost << '\n';
+            ost << om.vals[i].energy;
+        }           
+        
+        ost << '\n';
+        return ost;
+    }
 private:
-	inline Octopus &at(Point p) { return at(p.x, p.y); }
-	inline Octopus &at(int x, int y) { return vals[y * size + x]; }	
-	inline const Octopus &at(Point p) const { return at(p.x, p.y); }
-	inline const Octopus &at(int x, int y) const { return vals[y * size + x]; }
-	inline bool in_boundary(Point p) const { return in_boundary(p.x, p.y); }
-	inline bool in_boundary(int x, int y) const { 
-		return x >= 0 && y >= 0 && x < size && y < size;
-	}
-	
-	int do_flashes() {
-		int flashes = 0;
-		
-		for (int y = 0; y < size; ++y) {
-			for (int x = 0; x < size; ++x) {
-				Octopus &oct = at(x, y);
-				
-				if (!oct.flashed && oct.energy > 9) {
-					oct.energy = 0;
-					oct.flashed = true;
-					flashes += 1;
-					
-					for (const auto &pos : Point(x, y).neighbours())
-						if (in_boundary(pos))
-							at(pos).increment();
-				}
-			}
-		}
+    inline Octopus &at(Point p) { return at(p.x, p.y); }
+    inline Octopus &at(int x, int y) { return vals[y * size + x]; } 
+    inline const Octopus &at(Point p) const { return at(p.x, p.y); }
+    inline const Octopus &at(int x, int y) const { return vals[y * size + x]; }
+    inline bool in_boundary(Point p) const { return in_boundary(p.x, p.y); }
+    inline bool in_boundary(int x, int y) const { 
+        return x >= 0 && y >= 0 && x < size && y < size;
+    }
+    
+    int do_flashes() {
+        int flashes = 0;
+        
+        for (int y = 0; y < size; ++y) {
+            for (int x = 0; x < size; ++x) {
+                Octopus &oct = at(x, y);
+                
+                if (!oct.flashed && oct.energy > 9) {
+                    oct.energy = 0;
+                    oct.flashed = true;
+                    flashes += 1;
+                    
+                    for (const auto &pos : Point(x, y).neighbours())
+                        if (in_boundary(pos))
+                            at(pos).increment();
+                }
+            }
+        }
 
-		return flashes;
-	}
+        return flashes;
+    }
 };
 
 int main() {
-	OctopusMatrix matrix{};
-	std::cin >> matrix;
-	
-	matrix.step(100);
-	std::cout << matrix << '\n' << "total flashes = " << matrix.total() << '\n';
+    OctopusMatrix matrix{};
+    std::cin >> matrix;
+    
+    matrix.step(100);
+    std::cout << matrix << '\n' << "total flashes = " << matrix.total() << '\n';
 }
 
 
--- 21.1.cpp
+++ 21.1.cpp
@@ -2,90 +2,90 @@
 #include <array>
 
 constexpr int ten_turn_score(int player, int starting_pos) {
-	const int scores[2][10] {
-		{30, 40, 50, 60, 50, 60, 70, 80, 50, 60},
-		{55, 55, 45, 55, 65, 55, 55, 45, 55, 65},
-	};
-	
-	return scores[player - 1][starting_pos - 1];
+    const int scores[2][10] {
+        {30, 40, 50, 60, 50, 60, 70, 80, 50, 60},
+        {55, 55, 45, 55, 65, 55, 55, 45, 55, 65},
+    };
+    
+    return scores[player - 1][starting_pos - 1];
 }
 
 struct Die {
-	int result = -3, rolls = 0;
-	
-	int roll() {
-		rolls += 3;
-		switch (result += 9) {
-			case 300:
-				result = 0;
-				return 200;
-			case 303:
-				result = 3;
-				return 103;
-			case 306:
-				return result = 6;
-			default:
-				return result;
-		}
-	}
-	
-	void advance(int turns = 1) {
-		result = (result + 9 * turns) % 300;
-		rolls += 3 * turns;
-	}
-	
-	void reset() {
-		rolls = 0;
-		result = -3;
-	}
+    int result = -3, rolls = 0;
+    
+    int roll() {
+        rolls += 3;
+        switch (result += 9) {
+            case 300:
+                result = 0;
+                return 200;
+            case 303:
+                result = 3;
+                return 103;
+            case 306:
+                return result = 6;
+            default:
+                return result;
+        }
+    }
+    
+    void advance(int turns = 1) {
+        result = (result + 9 * turns) % 300;
+        rolls += 3 * turns;
+    }
+    
+    void reset() {
+        rolls = 0;
+        result = -3;
+    }
 };
 
 
 struct Pos {
-	static const int max_pos = 10;
-	int value;
-	
-	inline Pos operator+=(Pos p) {
-		return {value = ((value + p.value - 1) % max_pos + 1)};
-	}
-	
-	inline int operator+=(int n) {
-		return value = ((value + n - 1) % max_pos + 1);
-	}
+    static const int max_pos = 10;
+    int value;
+    
+    inline Pos operator+=(Pos p) {
+        return {value = ((value + p.value - 1) % max_pos + 1)};
+    }
+    
+    inline int operator+=(int n) {
+        return value = ((value + n - 1) % max_pos + 1);
+    }
 };
 
 std::array<int, 2> simulate(int p1_start_pos, int p2_start_pos, int turns, Die &die) {
-	Pos p1{p1_start_pos}, p2{p2_start_pos};
-	int score1 = (turns / 10) * ten_turn_score(1, p1_start_pos);
-	int score2 = (turns / 10) * ten_turn_score(2, p2_start_pos);
-	
-	die.advance((turns / 10) * 20);	
-	for (int i = 0; i < turns % 10; ++i) {
-		score1 += (p1 += die.roll());
-		if (score1 >= 1000) break;
-		score2 += (p2 += die.roll());
-		if (score2 >= 1000) break;
-	}
-	
-	return {score1, score2};
+    Pos p1{p1_start_pos}, p2{p2_start_pos};
+    int score1 = (turns / 10) * ten_turn_score(1, p1_start_pos);
+    int score2 = (turns / 10) * ten_turn_score(2, p2_start_pos);
+    
+    die.advance((turns / 10) * 20); 
+    for (int i = 0; i < turns % 10; ++i) {
+        score1 += (p1 += die.roll());
+        if (score1 >= 1000) break;
+        score2 += (p2 += die.roll());
+        if (score2 >= 1000) break;
+    }
+    
+    return {score1, score2};
 }
 
 void simulate(int p1_start_pos, int p2_start_pos, Die &die) {
-	const int turns = std::min(
-		10000 / ten_turn_score(1, p1_start_pos),
-		10000 / ten_turn_score(2, p2_start_pos)
-	);
-	
-	const auto result = simulate(p1_start_pos, p2_start_pos, turns + 1, die);
-	std::cout << "Player 1 score: " << result[0] << '\n'
-			  << "Player 2 score: " << result[1] << '\n'
-			  << "Rolls: " << die.rolls << '\n';
+    const int turns = std::min(
+        10000 / ten_turn_score(1, p1_start_pos),
+        10000 / ten_turn_score(2, p2_start_pos)
+    );
+    
+    const auto result = simulate(p1_start_pos, p2_start_pos, turns + 1, die);
+    std::cout << "Player 1 score: " << result[0] << '\n'
+              << "Player 2 score: " << result[1] << '\n'
+              << "Rolls: " << die.rolls << '\n';
 }
 
 int main() {
-	Die die{};
-	
-	simulate(1, 2, die);
+    Die die{};
+    
+    simulate(1, 2, die);
 }
 
 
